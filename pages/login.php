b08a77f3639ee744e233c693e498de64bddc735a
MoussaThiam6 <Moussat0987@gmail.com>
2023-11-10 14:50:38 +0000

    ajout formulaire  login

---
+++ pages/login.php
@@ -0,0 +1,19 @@
+<h2>Connexion</h2>
+<a href="../">Accueil</a>
+<form action="../result/Loginresult.php" method="post">
+    <!--Todou: change type-->
+    <label for="user_name">Nom d'utilisateur</label>
+    <input id="user_name" type="text" name="user_name" value="">
+    
+    <label for="pwd">password</label>
+    <input id="pwd" type="text" name="pwd" value="">
+
+    <button type="submit">Se connecter</button>
+    
+
+
+
+
+
+
+</form>
