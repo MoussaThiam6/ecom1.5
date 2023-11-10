6340ff16355207a8ba8c7e612f74dfa7e352c293
MoussaThiam6 <Moussat0987@gmail.com>
2023-11-10 14:51:00 +0000

    ajout formulaire signup

---
+++ pages/signup.php
@@ -0,0 +1,19 @@
+<h2>Enregistrement</h2>
+<a href="../">Accueil</a>
+<form action="../result/Signupresult.php" method="post">
+    <label for="user_name">Nom d'utilisateur</label>
+    <input id="user_name" type="text" name="user_name" value="">
+    <label for="email">email</label>
+    <input id="email" type="text" name="email" value="">
+    <label for="pwd">password</label>
+    <input id="pwd" type="text" name="pwd" value="">
+
+    <button type="submit">Enregistrer</button>
+    
+
+
+
+
+
+
+</form>
